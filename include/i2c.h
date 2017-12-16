3a15d8de839f82ec2a28ebc40cd4426b15ec3195
James-O-92 <james.overton467@gmail.com>
2017-12-16 13:04:31 +1300

    created i2c files

---
+++ include/i2c.h
@@ -0,0 +1,16 @@
+#ifndef I2C_H
+#define I2C_H
+
+
+class i2c
+{
+    public:
+        i2c();
+        virtual ~i2c();
+
+    protected:
+
+    private:
+};
+
+#endif // I2C_H
---
+++ src/i2c.cpp
@@ -0,0 +1,11 @@
+#include "i2c.h"
+
+i2c::i2c()
+{
+    //ctor
+}
+
+i2c::~i2c()
+{
+    //dtor
+}
