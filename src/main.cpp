689e8e420f47e7b6bfb260695961b519965999b1
James-O-92 <james.overton467@gmail.com>
2017-12-24 11:11:47 +1300

    -

--- src/main.cpp
+++ src/main.cpp
@@ -29,7 +29,7 @@ void i2c_test(i2c* i2c_bus)
 
     buffer[0] = 0b00000000;
     buffer[1] = 0b00000000;
-    strcpy(buffer,i2c_bus->read_register(addr,reg,2));
+    strcpy(buffer,(const char*)i2c_bus->read_register(addr,reg,2));
 
     printf("ADC conversion register: 0x%X 0x%X\n", buffer[0], buffer[1]);
 }
