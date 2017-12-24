1bb3544c132a119dc0569ae713a6572b653c8c60
James-O-92 <james.overton467@gmail.com>
2017-12-24 22:35:39 +1300

    -

--- src/DAC.cpp
+++ src/DAC.cpp
@@ -31,6 +31,6 @@ void DAC::updateVoltage(i2c* i2c_bus, float volts)
     }
 
     printf("voltage: %f, buf[0]: 0x%X, buf[1]: 0x%X",voltage,buf[0],buf[1]);
-    i2c_bus->write_register(addr,0x01,2,buffer);
+    i2c_bus->write_register(address,0x01,2,buffer);
 
 }
