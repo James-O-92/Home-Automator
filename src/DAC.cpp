dd0964ebadab520dfeffaf071f8b6273c0471f5a
James-O-92 <james.overton467@gmail.com>
2017-12-27 14:18:18 +1300

    Update DAC.cpp

--- src/DAC.cpp
+++ src/DAC.cpp
@@ -18,12 +18,11 @@ void DAC::updateVoltage(float volts)
 {
     unsigned char buffer[2];
 
-    //cout << "initializing i2c" << endl;
     i2c_bus->init("/dev/i2c-1");
 
     volts = volts/5;
     //cout << "%V = " << volts << endl;
-    if(volts <= 1)
+    if((volts <= 1) && (volts >= 0))
     {
         voltage = volts*5;
         volts = volts*4095;
@@ -32,9 +31,15 @@ void DAC::updateVoltage(float volts)
         buffer[1] = (unsigned char)(bin & 0xff);
         bin = bin >> 8;
         buffer[0] = bin;
+    }else
+    {
+        voltage = -1;
+        buffer[0] = 0;
+        buffer[1] = 0;
     }
 
-    //printf("voltage: %f, buf[0]: 0x%X, buf[1]: 0x%X\n",voltage,buffer[0],buffer[1]);
     i2c_bus->write_register(address,0x01,2,buffer);
+    
+    i2c_bus->i2c_close();
 
 }
